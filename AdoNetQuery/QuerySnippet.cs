d3e8c3a14506b5781704e1cc758d03aa195257b0
dmitry <dmitry.bondarenko@blueyounder.com>
2022-09-30 13:42:40 +0300

    build try 01

--- AdoNetQuery/QuerySnippet.cs
+++ AdoNetQuery/QuerySnippet.cs
@@ -16,9 +16,15 @@ namespace AdoNetQuery
 {
     public class QuerySnippet
     {
+        /// <summary>Name of code/SQL snippet</summary>
         public string Name { get; set; }
+
+        /// <summary>Description of snippet</summary>
         public string Description { get; set; }
+
+        /// <summary>The source code of snippet</summary>
         public string Query { get; set; }
+
         public string Parameters { get; set; }
 
         public void Assign(QuerySnippet pSource)
